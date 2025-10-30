b56293ce85c09582fde140449bd8c8d328948884
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2025-10-30 19:19:56 +0530

    temp patch constants

--- contract/programs/govcontract/src/constants.rs
+++ contract/programs/govcontract/src/constants.rs
@@ -3,7 +3,7 @@ pub const MAX_TITLE_LENGTH: usize = 50;
 pub const MAX_DESCRIPTION_LENGTH: usize = 250;
 
 // Voting limits
-pub const MAX_SUPPORT_EPOCHS: u64 = 10; // Maximum 10 epochs for support phase
+pub const MAX_SUPPORT_EPOCHS: u64 = 100; // Maximum 10 epochs for support phase
 
 pub const BASIS_POINTS_MAX: u64 = 10_000;
 
@@ -21,10 +21,10 @@ pub const MIN_PROPOSAL_STAKE_LAMPORTS: u64 = 1_000_000_000;
 pub const CLUSTER_SUPPORT_MULTIPLIER: u128 = 1;
 
 #[cfg(feature = "production")]
-pub const CLUSTER_SUPPORT_MULTIPLIER: u128 = 100;
+pub const CLUSTER_SUPPORT_MULTIPLIER: u128 = 1;
 
 #[cfg(feature = "testing")]
 pub const CLUSTER_STAKE_MULTIPLIER: u128 = 1;
 
 #[cfg(feature = "production")]
-pub const CLUSTER_STAKE_MULTIPLIER: u128 = 5;
+pub const CLUSTER_STAKE_MULTIPLIER: u128 = 1;
