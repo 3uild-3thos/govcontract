ffbe182c6e55698ed38ff0d9219a8f76a34a96af
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2025-10-31 20:49:35 +0530

    add meta merkle tree creation

--- svmgov/src/instructions/cast_vote.rs
+++ svmgov/src/instructions/cast_vote.rs
@@ -1,9 +1,12 @@
 use std::str::FromStr;
 
-use anchor_client::solana_sdk::{pubkey::Pubkey, signer::Signer};
+use anchor_client::solana_sdk::{
+    instruction::Instruction, pubkey::Pubkey, signer::Signer, transaction::Transaction,
+};
 use anchor_lang::system_program;
 use anyhow::{Result, anyhow};
-use gov_v1::ID as SNAPSHOT_PROGRAM_ID;
+use gov_v1::{ID as SNAPSHOT_PROGRAM_ID, MetaMerkleLeaf, MetaMerkleProof};
+use log::info;
 
 use crate::{
     constants::*,
@@ -35,7 +38,7 @@ pub async fn cast_vote(
     let proposal_pubkey = Pubkey::from_str(&proposal_id)
         .map_err(|_| anyhow!("Invalid proposal ID: {}", proposal_id))?;
 
-    let (payer, vote_account, program, _merkle_proof_program) =
+    let (payer, vote_account, program, merkle_proof_program) =
         setup_all(identity_keypair, rpc_url).await?;
 
     let proof_response =
@@ -50,7 +53,57 @@ pub async fn cast_vote(
 
     let spinner = create_spinner("Sending cast-vote transaction...");
 
-    let sig = program
+    let mut ixs: Vec<Instruction> = vec![];
+
+    // Check if meta merkle proof account exists, create if missing
+    let meta_merkle_proof_account = match program
+        .account::<MetaMerkleProof>(meta_merkle_proof_pda)
+        .await
+    {
+        Ok(account) => Some(account),
+        Err(_e) => {
+            info!("Unable to get meta merkle proof account, will create it");
+            None
+        }
+    };
+
+    if meta_merkle_proof_account.is_none() {
+        info!("Creating meta merkle proof account");
+
+        let voting_wallet = Pubkey::from_str(&proof_response.meta_merkle_leaf.voting_wallet)
+            .map_err(|e| anyhow!("Invalid voting wallet in proof: {}", e))?;
+
+        let init_meta_merkle_proof_ix = merkle_proof_program
+            .request()
+            .args(gov_v1::instruction::InitMetaMerkleProof {
+                close_timestamp: 1,
+                meta_merkle_leaf: MetaMerkleLeaf {
+                    voting_wallet,
+                    vote_account,
+                    stake_merkle_root: Pubkey::from_str_const(
+                        proof_response.meta_merkle_leaf.stake_merkle_root.as_str(),
+                    )
+                    .to_bytes(),
+                    active_stake: proof_response.meta_merkle_leaf.active_stake,
+                },
+                meta_merkle_proof: proof_response
+                    .meta_merkle_proof
+                    .iter()
+                    .map(|s| Pubkey::from_str_const(s).to_bytes())
+                    .collect(),
+            })
+            .accounts(gov_v1::accounts::InitMetaMerkleProof {
+                consensus_result: consensus_result_pda,
+                merkle_proof: meta_merkle_proof_pda,
+                payer: payer.pubkey(),
+                system_program: system_program::ID,
+            })
+            .instructions()?;
+
+        ixs.extend(init_meta_merkle_proof_ix);
+    }
+
+    let cast_vote_ixs = program
         .request()
         .args(args::CastVote {
             for_votes_bp: votes_for,
@@ -68,7 +121,17 @@ pub async fn cast_vote(
             snapshot_program: SNAPSHOT_PROGRAM_ID,
             system_program: system_program::ID,
         })
-        .send()
+        .instructions()?;
+
+    ixs.extend(cast_vote_ixs);
+
+    let blockhash = program.rpc().get_latest_blockhash().await?;
+    let transaction =
+        Transaction::new_signed_with_payer(&ixs, Some(&payer.pubkey()), &[&payer], blockhash);
+
+    let sig = program
+        .rpc()
+        .send_and_confirm_transaction(&transaction)
         .await?;
 
     spinner.finish_with_message(format!(
--- svmgov/src/instructions/cast_vote_override.rs
+++ svmgov/src/instructions/cast_vote_override.rs
@@ -1,9 +1,11 @@
 use std::str::FromStr;
 
-use anchor_client::solana_sdk::{pubkey::Pubkey, signer::Signer};
+use anchor_client::solana_sdk::{
+    instruction::Instruction, pubkey::Pubkey, signer::Signer, transaction::Transaction,
+};
 use anchor_lang::system_program;
 use anyhow::{Result, anyhow};
-use gov_v1::ID as SNAPSHOT_PROGRAM_ID;
+use gov_v1::{ID as SNAPSHOT_PROGRAM_ID, MetaMerkleLeaf, MetaMerkleProof};
 use log::info;
 
 use crate::{
@@ -46,7 +48,7 @@ pub async fn cast_vote_override(
     let proposal_pubkey = Pubkey::from_str(&proposal_id)
         .map_err(|_| anyhow!("Invalid proposal ID: {}", proposal_id))?;
 
-    let (payer, program, _merkle_proof_program) = setup_all_with_staker(staker_keypair, rpc_url)?;
+    let (payer, program, merkle_proof_program) = setup_all_with_staker(staker_keypair, rpc_url)?;
 
     let stake_account_str = stake_account_override.clone();
     let vote_account_pubkey = Pubkey::from_str(&vote_account)
@@ -78,7 +80,60 @@ pub async fn cast_vote_override(
 
     let spinner = create_spinner("Sending vote override transaction...");
 
-    let sig = program
+    let mut ixs: Vec<Instruction> = vec![];
+
+    // Check if meta merkle proof account exists, create if missing
+    let meta_merkle_proof_account = match program
+        .account::<MetaMerkleProof>(meta_merkle_proof_pda)
+        .await
+    {
+        Ok(account) => Some(account),
+        Err(_e) => {
+            info!("Unable to get meta merkle proof account, will create it");
+            None
+        }
+    };
+
+    if meta_merkle_proof_account.is_none() {
+        info!("Creating meta merkle proof account");
+
+        let voting_wallet = Pubkey::from_str(&meta_merkle_proof.meta_merkle_leaf.voting_wallet)
+            .map_err(|e| anyhow!("Invalid voting wallet in proof: {}", e))?;
+
+        let init_meta_merkle_proof_ix = merkle_proof_program
+            .request()
+            .args(gov_v1::instruction::InitMetaMerkleProof {
+                close_timestamp: 1,
+                meta_merkle_leaf: MetaMerkleLeaf {
+                    voting_wallet,
+                    vote_account: vote_account_pubkey,
+                    stake_merkle_root: Pubkey::from_str_const(
+                        meta_merkle_proof
+                            .meta_merkle_leaf
+                            .stake_merkle_root
+                            .as_str(),
+                    )
+                    .to_bytes(),
+                    active_stake: meta_merkle_proof.meta_merkle_leaf.active_stake,
+                },
+                meta_merkle_proof: meta_merkle_proof
+                    .meta_merkle_proof
+                    .iter()
+                    .map(|s| Pubkey::from_str_const(s).to_bytes())
+                    .collect(),
+            })
+            .accounts(gov_v1::accounts::InitMetaMerkleProof {
+                consensus_result: consensus_result_pda,
+                merkle_proof: meta_merkle_proof_pda,
+                payer: payer.pubkey(),
+                system_program: system_program::ID,
+            })
+            .instructions()?;
+
+        ixs.extend(init_meta_merkle_proof_ix);
+    }
+
+    let cast_vote_override_ixs = program
         .request()
         .args(args::CastVoteOverride {
             for_votes_bp: for_votes,
@@ -100,7 +155,17 @@ pub async fn cast_vote_override(
             snapshot_program: SNAPSHOT_PROGRAM_ID,
             system_program: system_program::ID,
         })
-        .send()
+        .instructions()?;
+
+    ixs.extend(cast_vote_override_ixs);
+
+    let blockhash = program.rpc().get_latest_blockhash().await?;
+    let transaction =
+        Transaction::new_signed_with_payer(&ixs, Some(&payer.pubkey()), &[&payer], blockhash);
+
+    let sig = program
+        .rpc()
+        .send_and_confirm_transaction(&transaction)
         .await?;
 
     spinner.finish_with_message(format!(
