54c944cc037787ce8e774f538b4896d7af1311da
JuanMarchetto <juanpatriciomarchetto@gmail.com>
2025-11-02 11:39:15 -0300

    fix

--- frontend/src/chain/types/govcontractV2.ts
+++ frontend/src/chain/types/govcontractV2.ts
@@ -5,7 +5,7 @@
  * IDL can be found at `target/idl/govcontract.json`.
  */
 export type Govcontract = {
-  "address": "DUWGu3sMy4ymJWwUs53eaCyNzZRFoxhmi3Ggf1kh8Q61",
+  "address": "12ZGhCoEAGdStDJCzxZT9Vbn3qTW6VprH4GkvXcErZmT",
   "metadata": {
     "name": "govcontract",
     "version": "0.1.0",
