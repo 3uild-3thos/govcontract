90a795ccb4794e28ec4a260a9304bf6b68885615
Pedro Marques <pedrommarquesm@gmail.com>
2025-11-06 18:42:00 +0000

    fe: small lint cleanup

--- frontend/src/components/AppWalletProvider.tsx
+++ frontend/src/components/AppWalletProvider.tsx
@@ -1,11 +1,10 @@
 "use client";
 
-import React, { useMemo } from "react";
+import { ReactNode, useMemo } from "react";
 import {
   ConnectionProvider,
   WalletProvider,
 } from "@solana/wallet-adapter-react";
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 // import { UnsafeBurnerWalletAdapter } from "@solana/wallet-adapter-wallets";
 import { useEndpoint } from "@/contexts/EndpointContext";
@@ -16,17 +15,16 @@ import "@solana/wallet-adapter-react-ui/styles.css";
 export default function AppWalletProvider({
   children,
 }: {
-  children: React.ReactNode;
+  children: ReactNode;
 }) {
   const { endpointUrl } = useEndpoint();
-  const network = WalletAdapterNetwork.Devnet;
 
   const wallets = useMemo(
     () => [
       // manually add any legacy wallet adapters here
       // new UnsafeBurnerWalletAdapter(),
     ],
-    [network]
+    []
   );
 
   return (
